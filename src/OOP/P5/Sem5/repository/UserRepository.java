745151b06deb8d37814cdd5abb9171f3239b0230
Eugene <palevich86@mail.ru>
2023-01-15 18:48:13 +0300

    Delete src/OOP/P5/Sem5 directory

--- src/OOP/P5/Sem5/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package OOP.P5.Sem5.repository;
-
-import OOP.P5.Sem5.data.User;
-
-public interface UserRepository<E extends User, I> extends Repository<E, I> {
-    E findByLastName(String lastName);
-
-}
